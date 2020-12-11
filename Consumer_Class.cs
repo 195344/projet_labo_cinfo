fe2deb459ace4317b145adc4dd2ab11d1ef4b2f8
LeTouristeDeLECAM <75834946+LeTouristeDeLECAM@users.noreply.github.com>
2020-12-11 18:50:59 +0100

    ajout class consumer

---
+++ Consumer_Class.cs
@@ -0,0 +1,75 @@
+﻿namespace Consumer
+
+    //interface 
+
+public abstract class Consumer	
+{
+	
+
+
+        private int _quantityEnergie;
+        
+        private string typeCons;
+        private string localization;
+
+
+        public Consumer(int _quantityEnergie, string localization)
+        {
+            this.typeCons = typeCons;
+            this.connexion = connexion;
+        }
+
+        public int QuantityEnergie
+        {
+            get { return _quantityEnergie; }
+            set { _quantityEnergie = value; }
+        }
+
+
+
+}
+
+public class City : Consumer
+{
+
+    private string nameCity;
+     
+
+    public Ville (:,string nameCity)
+    {
+        this.nameCity = nameCity; 
+
+    }
+
+
+}
+
+
+
+
+public class Entreprise : Consumer
+{
+
+    private string nameCompany;
+
+    public Entreprise(:, string nameCompany)
+    {
+        this.nameCompany = nameCompany;
+
+    }
+
+
+}
+
+public class Dissipateur : Consumer
+{
+
+
+    public Dissipateur()
+    {
+        
+    }
+
+
+}
+
