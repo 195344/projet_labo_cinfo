5a27d931d923f63aee4351e0665f00261812f3d9
195344 <60741544+195344@users.noreply.github.com>
2020-12-11 09:48:38 +0100

    ajout producteur

---
+++ Producteur.cs
@@ -0,0 +1,34 @@
+namespace Prod
+{
+    interface Producteur{
+        
+        double Production { get; }
+        double Cout { get; }
+        double Emission { get; }
+    }
+
+    public class Centrale_Gaz : Producteur{
+        private double production;
+        private double cout;
+        private double emission;
+
+        public Centrale_Gaz(double production, double cout, double emission){
+            this.production = production;
+            this.cout = cout;
+            this.emission = emission;
+        }
+
+        public double Production{
+            get {return production;}
+            set {production = value;}
+        } 
+
+        public double Cout{
+            get {return cout;}
+        } 
+
+        public double Emission{
+            get {return emission;}
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Prod;
+
+namespace Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producteur myprod = new Centrale_Gaz(1,1,1);
+            Console.WriteLine(myprod.Production);
+        }
+    }
+}
